656ffcdb367570a3abf404378f44e2764f271ec3
Djibsonx7 <59049134+Djibsonx7@users.noreply.github.com>
2026-05-09 16:11:41 +0000

    Add neutral title for quote page

---
+++ app/devis/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>FIX Dépannage Dakar | Devis digital</title>
+      <meta name="description" content="Générateur de devis digital FIX Dépannage Dakar." />
+    </>
+  );
+}
